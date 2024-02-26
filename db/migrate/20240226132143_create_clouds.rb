4b6e3b535d36d817eab9dd9fb4dd2bbfa83481e6
Viorel Borgovan <viorel.borgovan@gmail.com>
2024-02-26 14:29:24 +0100

    created models

---
+++ db/migrate/20240226132143_create_clouds.rb
@@ -0,0 +1,14 @@
+class CreateClouds < ActiveRecord::Migration[7.1]
+  def change
+    create_table :clouds do |t|
+      t.string :name
+      t.string :type
+      t.text :description
+      t.string :address
+      t.date :available_from
+      t.date :available_until
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_26_132827) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "users_id", null: false
+    t.bigint "clouds_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["clouds_id"], name: "index_bookings_on_clouds_id"
+    t.index ["users_id"], name: "index_bookings_on_users_id"
+  end
+
+  create_table "clouds", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.text "description"
+    t.string "address"
+    t.date "available_from"
+    t.date "available_until"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "bookings", "clouds", column: "clouds_id"
+  add_foreign_key "bookings", "users", column: "users_id"
 end
