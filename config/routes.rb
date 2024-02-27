a020a86cb3235c0a4cd728c9973b84a62a88d3c3
Alfonso Xavier <alfonso.xavier@icloud.com>
2024-02-27 11:37:28 +0100

    integrated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,24 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :clouds, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:edit]
+    # resources :users, only: [:new, :create, :edit, :destroy]
 end
+
+# /	GET	clouds	index✅
+# clouds/:id	GET	clouds	show ✅
+# clouds/new	GET	clouds	new ✅
+# clouds/create	POST	clouds	create ✅
+# clouds/:id/edit	GET	clouds	edit ✅
+# clouds/:id	PATCH	coulds	update ✅
+# clouds/:id	DELETE	coulds	destroy ✅
+
+# /clouds/:cloud_id/new	GET	bookings	new
+# /clouds/:cloud_id/bookings	POST	bookings	create
+
+# bookings/index	GET	bookings	bookings home
+# bookings/:id	GET	bookings	nookings show
