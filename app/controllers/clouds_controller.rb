67e59ae7f99b6528df52a4e2ca4a619e6d36f9fe
Alfonso Xavier <alfonso.xavier@icloud.com>
2024-02-27 14:13:46 +0100

    sign in and log in features

--- app/controllers/clouds_controller.rb
+++ app/controllers/clouds_controller.rb
@@ -1,5 +1,8 @@
 class CloudsController < ApplicationController
+  # skip_before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_cloud, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, except: [:index, ]
 
   def index
     @clouds = Cloud.all
@@ -49,4 +52,3 @@ class CloudsController < ApplicationController
     params.require(:cloud).permit(:name, :category, :description, :address)
   end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,5 @@ Rails.application.routes.draw do
   end
   resources :bookings, only: [:edit]
     # resources :users, only: [:new, :create, :edit, :destroy]
+
 end
-
-# /	GET	clouds	index✅
-# clouds/:id	GET	clouds	show ✅
-# clouds/new	GET	clouds	new ✅
-# clouds/create	POST	clouds	create ✅
-# clouds/:id/edit	GET	clouds	edit ✅
-# clouds/:id	PATCH	coulds	update ✅
-# clouds/:id	DELETE	coulds	destroy ✅
-
-# /clouds/:cloud_id/new	GET	bookings	new
-# /clouds/:cloud_id/bookings	POST	bookings	create
-
-# bookings/index	GET	bookings	bookings home
-# bookings/:id	GET	bookings	nookings show
