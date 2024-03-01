c65b447cf69162bf23f3fe2e4e797f62a8b1ed56
Viorel Borgovan <viorel.borgovan@gmail.com>
2024-03-01 14:25:23 +0100

    commented geocoding out

--- app/models/cloud.rb
+++ app/models/cloud.rb
@@ -1,13 +1,12 @@
 class Cloud < ApplicationRecord
   belongs_to :user
 #  geocoded_by :address
-  before_validation :sanitize_picture_url, on: :create
+  after_create :sanitize_picture_url
   after_validation :geocode, if: :will_save_change_to_address?
   validates :description, length: { minimum: 220 }
 
-  private
-
   def sanitize_picture_url
-    self.picture_url ||= "https://i.ibb.co/6mHKxVW/default.png"
+    self.picture_url = "https://i.ibb.co/6mHKxVW/default.png" if self.picture_url.empty?
+    self.save
   end
 end
