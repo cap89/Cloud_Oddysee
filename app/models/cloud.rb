c5717c07737e9544145a1fc0c4b3c77195cbbf00
Martin <155843922+cap89@users.noreply.github.com>
2024-03-01 15:00:22 +0100

    last fixes

--- app/models/cloud.rb
+++ app/models/cloud.rb
@@ -3,7 +3,9 @@ class Cloud < ApplicationRecord
 #  geocoded_by :address
   after_create :sanitize_picture_url
 #  after_validation :geocode, if: :will_save_change_to_address?
-  validates :description, length: { maximum: 220 }
+  validates :description, length: { maximum: 220 }, presence: true
+  validates :latitude, :longitude, :address, :available_from, :available_until, :name, presence: true
+
 
   def sanitize_picture_url
     self.picture_url = "https://i.ibb.co/6mHKxVW/default.png" if self.picture_url.empty?
